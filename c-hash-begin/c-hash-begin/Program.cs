2925867d9608032a8fb658401b5a4e41e752ad66
Tanaya Vadgave <tanaya13693@Tanayas-MBP.fios-router.home>
2020-04-02 01:17:54 -0400

    Hello World

---
+++ c-hash-begin/c-hash-begin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_hash_begin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
