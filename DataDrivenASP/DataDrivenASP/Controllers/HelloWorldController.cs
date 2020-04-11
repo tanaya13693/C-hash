c21a651b3a9f46ebf2225920900139034228cca1
Tanaya Vadgave <tanaya13693@Tanayas-MBP.fios-router.home>
2020-04-10 23:26:19 -0400

    CRUD

--- DataDrivenASP/DataDrivenASP/Controllers/HelloWorldController.cs
+++ DataDrivenASP/DataDrivenASP/Controllers/HelloWorldController.cs
@@ -14,19 +14,20 @@ namespace DataDrivenASP.Controllers
         // 
         // GET: /HelloWorld/
         //Example: localhost:5001/HelloWorld
-
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
         // 
         // GET: /HelloWorld/Welcome/ 
         //Example: localhost:5001/HelloWorld/Welcome?name=Tanaya&numTimes=5
-
-        public string Welcome(string name, int numTimes = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
 
     }
